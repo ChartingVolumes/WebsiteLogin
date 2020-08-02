806606c5c60a51b05fecd3f4882a4c5d673a6e65
ChartingVolumes <36436810+ChartingVolumes@users.noreply.github.com>
2020-08-03 01:20:57 +1000

    Update form.js

--- Form/form.js
+++ Form/form.js
@@ -69,7 +69,6 @@
     function googleSignIn() {
         var provider = new firebase.auth.GoogleAuthProvider();
         provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
-        firebase.auth().useDeviceLanguage();
         firebase.auth().signInWithPopup(provider).then(function(result) {
             // This gives you a Google Access Token. You can use it to access the Google API.
             var token = result.credential.accessToken;
